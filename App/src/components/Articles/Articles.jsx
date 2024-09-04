e49e666609cf1dbd325377aff2c1549566732a2b
NicolaiSzuwalski <132432642+NicolaiSzuwalski@users.noreply.github.com>
2024-09-04 23:09:32 +0200

    articles halvejs

--- App/src/components/Articles/Articles.jsx
+++ App/src/components/Articles/Articles.jsx
@@ -3,10 +3,17 @@ import styles from './Articles.module.scss'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { useSupabase } from '../../providers/SupabaseProvider'
+import readMore from '../../assets/images/readMore.png'
+import { format } from 'date-fns';
 
 export const Articles = () => {
     const { supabase } = useSupabase();
     const [data, setData] = useState([]);
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return format(date, 'dd. MMMM yyyy');
+    };
 
     const getArticleData = async () => {
         if(supabase){
@@ -28,6 +35,29 @@ export const Articles = () => {
 
 
   return (
-    <div>Articles</div>
+    <section className={styles.ArticlesMain}>
+        <h1>Artikler</h1>
+        <div className={styles.ArticleCards}>
+            {data.map((item) =>{
+                return(
+                <div key={item.id} className={styles.Card}>
+
+                    <div className={styles.cardImg}>
+                        <img src={item.image_url} alt="ArticleImage" />
+                    </div>
+
+                    <div className={styles.Text}>
+                    <div>
+                    <h4>{item.title}</h4>
+                    <b>{formatDate(item.published_at)}</b>
+                    </div>
+                    <p>{item.teaser}</p>
+                    <Link to='/:section_id'><img src={readMore} alt="readMore" /></Link>
+                    </div>
+                </div>
+                );
+            })}
+        </div>
+    </section>
   )
 }
--- App/src/pages/ArticlesPage/ArticlesPage.jsx
+++ App/src/pages/ArticlesPage/ArticlesPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { GradientWrapper } from '../../components/GradientWrapper/GradientWrapper'
 import { Articles } from '../../components/Articles/Articles'
+import { ContentWrapper } from '../../components/ContentWrapper/ContentWrapper'
 
 export const ArticlesPage = () => {
   return (
     <GradientWrapper>
+      <ContentWrapper>
         <Articles></Articles>
+      </ContentWrapper>
     </GradientWrapper>
   )
 }
